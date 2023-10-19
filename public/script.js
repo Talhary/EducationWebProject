3d8153040c22109d6c11db42b7a775b848e94e1e
Talhary <talhariaz5425869@gmail.com>
2023-10-19 09:45:55 +0500

    fds

--- public/script.js
+++ public/script.js
@@ -53,50 +53,52 @@ btn1.addEventListener('click', () => {
 // I. Introduction
 
 // Course Title: Numerical Analysis
+
+// Course Title: Numerical Analysis
 addDataToMain("Course Title: Numerical Analysis", `
-    <strong>Course Outline:</strong>
+    <strong>Course Outline:</strong><br><br>
     <ol>
-        <li><strong>I. Introduction</strong>
+        <li><strong>I. Introduction</strong><br>
             <ul>
                 <li>Overview of Numerical Analysis</li>
                 <li>Importance of Numerical Methods</li>
                 <li>Applications in Science and Engineering</li>
             </ul>
-        </li>
+        </li><br>
 
-        <li><strong>II. Computer Arithmetic</strong>
+        <li><strong>II. Computer Arithmetic</strong><br>
             <ul>
                 <li>Number Systems (Binary, Octal, Hexadecimal)</li>
                 <li>Conversion between Number Systems</li>
                 <li>Binary Arithmetic</li>
             </ul>
-        </li>
+        </li><br>
 
-        <li><strong>III. Errors and Their Types</strong>
+        <li><strong>III. Errors and Their Types</strong><br>
             <ul>
                 <li>Sources of Errors in Numerical Computations</li>
                 <li>Absolute and Relative Errors</li>
                 <li>Types of Errors: Truncation and Round-off Errors</li>
             </ul>
-        </li>
+        </li><br>
 
-        <li><strong>IV. Floating Point Arithmetic</strong>
+        <li><strong>IV. Floating Point Arithmetic</strong><br>
             <ul>
                 <li>IEEE 754 Standard for Floating Point Representation</li>
                 <li>Rounding Errors in Floating-Point Operations</li>
                 <li>Overflow and Underflow</li>
             </ul>
-        </li>
+        </li><br>
 
-        <li><strong>V. Nonlinear Equations</strong>
+        <li><strong>V. Nonlinear Equations</strong><br>
             <ul>
                 <li>Introduction to Nonlinear Equations</li>
                 <li>Direct and Indirect Methods for Solving Nonlinear Equations</li>
                 <li>Intermediate Value Theorem</li>
             </ul>
-        </li>
+        </li><br>
 
-        <li><strong>VI. Iterative Methods for Nonlinear Equations</strong>
+        <li><strong>VI. Iterative Methods for Nonlinear Equations</strong><br>
             <ul>
                 <li>Secant Method</li>
                 <li>Regula-Falsi Method</li>
@@ -104,39 +106,39 @@ addDataToMain("Course Title: Numerical Analysis", `
                 <li>Iterative Methods Based on Second Degree Equations</li>
                 <li>Rate of Convergence for Iterative Methods</li>
             </ul>
-        </li>
+        </li><br>
 
-        <li><strong>VII. General Iterative Methods</strong>
+        <li><strong>VII. General Iterative Methods</strong><br>
             <ul>
                 <li>First, Second, and Higher Order Iterative Methods</li>
                 <li>Acceleration of Convergence Techniques</li>
                 <li>Methods for Handling Multiple Roots</li>
             </ul>
-        </li>
+        </li><br>
 
-        <li><strong>VIII. System of Nonlinear Equations</strong>
+        <li><strong>VIII. System of Nonlinear Equations</strong><br>
             <ul>
                 <li>Introduction to Systems of Nonlinear Equations</li>
                 <li>Methods for Solving Systems of Nonlinear Equations</li>
             </ul>
-        </li>
+        </li><br>
 
-        <li><strong>IX. Polynomial Equations</strong>
+        <li><strong>IX. Polynomial Equations</strong><br>
             <ul>
                 <li>Techniques for Solving Polynomial Equations</li>
             </ul>
-        </li>
+        </li><br>
 
-        <li><strong>X. System of Linear Algebraic Equations</strong>
+        <li><strong>X. System of Linear Algebraic Equations</strong><br>
             <ul>
                 <li>Introduction to Linear Algebraic Equations</li>
-                <li>Direct Methods:
+                <li>Direct Methods:<br>
                     <ul>
                         <li>Gauss Elimination Methods with Partial and Complete Pivoting</li>
                         <li>Gauss-Jordan Elimination Method</li>
                     </ul>
                 </li>
-                <li>Triangularization Methods:
+                <li>Triangularization Methods:<br>
                     <ul>
                         <li>Doolittle’s Method</li>
                         <li>Crout’s Method</li>
@@ -144,7 +146,7 @@ addDataToMain("Course Title: Numerical Analysis", `
                     </ul>
                 </li>
                 <li>Partition Methods</li>
-                <li>Indirect Methods:
+                <li>Indirect Methods:<br>
                     <ul>
                         <li>Jacobi Iteration Method</li>
                         <li>Gauss-Seidel Iteration Method</li>
@@ -154,14 +156,14 @@ addDataToMain("Course Title: Numerical Analysis", `
                 <li>Convergence of Iterative Methods</li>
                 <li>Optimal Relaxation Parameter for SOR Method</li>
             </ul>
-        </li>
+        </li><br>
 
-        <li><strong>XI. Solution of Tridiagonal Systems</strong></li>
+        <li><strong>XI. Solution of Tridiagonal Systems</strong></li><br>
 
-        <li><strong>XII. Interpolation</strong>
+        <li><strong>XII. Interpolation</strong><br>
             <ul>
                 <li>Introduction to Interpolation</li>
-                <li>Finite Difference Operators:
+                <li>Finite Difference Operators:<br>
                     <ul>
                         <li>Forward, Backward, Central Differences</li>
                         <li>Shift, Average, and Differential Operators</li>
@@ -172,7 +174,7 @@ addDataToMain("Course Title: Numerical Analysis", `
                 <li>Error Propagation in Difference Tables</li>
                 <li>Summation of Series</li>
                 <li>Newton’s Formulae for Interpolation</li>
-                <li>Interpolation with Unequally Spaced Data:
+                <li>Interpolation with Unequally Spaced Data:<br>
                     <ul>
                         <li>Lagrange’s Formula and Its Error</li>
                         <li>Hermite’s Interpolation Formula</li>
