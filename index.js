fd238847be622258e5db724ffaff1c285a33373f
Talhary <talhariaz5425869@gmail.com>
2023-10-19 09:25:30 +0500

    updated

--- index.js
+++ index.js
@@ -2,13 +2,19 @@ const express = require('express');
 const app = express();
 require('dotenv').config();
 const port = process.env.PORT || 5000;
+// Third party libraries
+const helmet = require('helmet');
+const cors = require('cors');
+//
+const NotFound = require('./middleware/notFound.js');
 //
 const NotFound = require('./middleware/notFound.js');
 const Errors = require('./errors/ErrorsHandling.js')
 //
 app.use(express.json())
 app.use(express.static('./public'))
-
+app.use(helmet());
+app.use(cors());
 app.use(Errors);
 app.use(NotFound);
 //
